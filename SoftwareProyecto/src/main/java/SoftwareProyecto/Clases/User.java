365c2279b32273c87a82b1f8ba409099ed2b038d
Alejandro Martínez Quiroga <153730088+Al3xM4rki@users.noreply.github.com>
2024-11-05 16:10:36 +0100

    added password to user and added roles in user for later use

--- SoftwareProyecto/src/main/java/SoftwareProyecto/Clases/User.java
+++ SoftwareProyecto/src/main/java/SoftwareProyecto/Clases/User.java
@@ -1,5 +1,6 @@
 package SoftwareProyecto.Clases;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import java.util.List;
 
@@ -11,16 +12,34 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @JsonIgnore
+    private String password;
     private String username;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Password> passwords;
+
+    /*
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles;
+    */
 
     public User() {}
 
     public User(String username) {
         this.username = username;
     }
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    /* Para posteriores roles
+
+    public void setRoles(List<String> roles){this.roles = roles;}
+    public List<String> getRoles(){return this.roles;}
+    */
 
     public Long getId() {
         return id;
